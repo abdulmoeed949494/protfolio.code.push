a5cecc8d9193bb8449c2cfba96d554951dc1ade6
abdulmoeed949494 <159937546+abdulmoeed9494@users.noreply.github.com>
2024-07-10 17:39:26 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Skills from "./Skills";
 import Contact from "./Contact";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
-const App = () => {
+export default function App() {
+
+  const [myStyle, setMyStyle] = useState({
+    color: "white",
+    backgroundColor: "black"
+  })
+
+  const [btntext, setBtnText] = useState(<MdDarkMode />)
+
+  const toggleStyle = () => {
+    if(myStyle.color === "white"){
+      setMyStyle({
+          color: "black",
+          backgroundColor: "white"
+      })
+      setBtnText(<MdLightMode />)
+    }
+    else {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black"
+    })
+    setBtnText(<MdDarkMode />);
+    }
+  }
+
   return (
     <Router>
       <nav>
-        <div className="btnmain">
+        <div className="btnmain" style={myStyle}>
           <Link className="homebtn" to="/"><button>Home</button></Link>
           <Link className="aboutbtn" to="/about"><button>About</button></Link>
           <Link className="skillsbtn" to="/skills"><button>Skills</button></Link>
           <Link className="contactbtn" to="/contact"><button>Contact</button></Link>
+          <div className=''>
+            <button onClick={toggleStyle} type='button' className='btnmode'>{btntext}</button>
+          </div>
         </div>
       </nav>
       <Routes>
@@ -25,6 +55,3 @@ const App = () => {
     </Router>
   );
 };
-
-export default App;
-
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import image from './assets/image.jpg';
 
-const Home = () => {
+export default function Home() {
+
   return (
     <div className='home'>
       <div className='homemain'>
@@ -23,9 +23,94 @@ const Home = () => {
           <div class="animation2"></div>
         </div>
       </div>
+
     </div>
   )
 }
 
-export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React, { useState } from 'react';
+// import image from './assets/image.jpg';
+
+// export default function Home() {
+
+//   // const [myStyle, setMyStyle] = useState({
+//   //   color: "white",
+//   //   backgroundColor: "black"
+//   // })
+
+//   // const [btntext, setBtnText] = useState("Enable Dark Mode")
+
+//   // const toggleStyle = () => {
+//   //   if(myStyle.color === "white"){
+//   //     setMyStyle({
+//   //         color: "black",
+//   //         backgroundColor: "white"
+//   //     })
+//   //     setBtnText("Enable Light Mode")
+//   //   }
+//   //   else {
+//   //     setMyStyle({
+//   //       color: "white",
+//   //       backgroundColor: "black"
+//   //   })
+//   //   setBtnText("Enable Dark Mode");
+//   //   }
+//   // }
+
+//   return (
+//     <div className='home'>
+//       <div className='homemain'>
+// {/* <div className='setdesign'> */}
+//         <p className='hp'>Hey I am</p>
+//         <div className='hhdiv'><h1 className='hhone'>Front</h1><h1 className='hhtwo'>end</h1></div>
+//         <h1 className='hh'>Developer</h1>
+//         <p className='pp'>I am frontend developer based in Pakistan, i will help you build beautiful websites your users will love</p>
+//         <div className='homebtnjs'>
+//           <button className="btnhomeone"><a href='https://www.indeed.com/career-advice/finding-a-job/how-to-get-a-tech-job'>Get In Tech</a></button>
+//           <button className="btnhometwo"><a href='https://www.khanacademy.org/computing/computer-programming/browse'>Browse Projects</a></button>
+//         </div>
+// {/* </div> */}
+//       </div>
+
+//       <div className='rightdiv'>
+//         <div className='righttwodiv'>
+//           <div>
+//             <img className='image' src={image} alt="image" />
+//           </div>
+//           <div class="animation2"></div>
+//         </div>
+//       </div>
+
+// {/* <div className=''>
+//     <button onClick={toggleStyle} type='button' className=''>{btntext}</button>
+// </div> */}
+
+//     </div>
+//   )
+// }
+
+
 
